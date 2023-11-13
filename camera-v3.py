93110a21154aefb5e979eec7da83c8bca9b30229
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2023-11-12 22:29:12 -0500

    Create camera-v3.py

---
+++ camera-v3.py
@@ -0,0 +1,47 @@
+from picamera2.encoders import H264Encoder
+from picamera2 import Picamera2
+from libcamera import Transform
+import time
+import os
+import glob
+import subprocess
+
+video_length_seconds = 10 * 60  # 10 minutes
+max_videos = 1000
+video_path = "/videos"  # Replace with your desired path
+
+# Ensure video directory exists
+if not os.path.exists(video_path):
+    os.makedirs(video_path)
+
+# Initialize camera
+picam2 = Picamera2()
+#picam2.pre_callback = apply_timestamp()
+video_config = picam2.create_video_configuration(transform=Transform(rotation=0))
+#video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+# Function to manage video files
+def trim_videos():
+    all_videos = sorted(glob.glob(os.path.join(video_path, "*.mp4")))
+    while len(all_videos) > max_videos:
+        os.remove(all_videos.pop(0))  # Remove the oldest video
+
+# Main loop
+while True:
+    timestamp = int(time.time())
+    h264_filename = os.path.join(video_path, f'video_{timestamp}.h264')
+    mp4_filename = os.path.join(video_path, f'video_{timestamp}.mp4')
+
+    # Start recording
+    encoder = H264Encoder(bitrate=100000)  # Adjust bitrate as needed
+    picam2.start_recording(encoder, h264_filename)
+    time.sleep(video_length_seconds)
+    picam2.stop_recording()
+
+    # Convert to MP4
+    subprocess.run(["MP4Box", "-add", h264_filename, mp4_filename])
+    os.remove(h264_filename)
+
+    # Trim older videos
+    trim_videos()
